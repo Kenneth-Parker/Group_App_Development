e3ff7d1b981ebc4f9ec1b8a07971d9ece8428da9
anickacodes <anickalewis@pursuit.org>
2023-09-09 17:14:13 -0400

    installed Browser Router collective

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,17 @@
+import {BrowserRouter as Router, Routes, Route, Link} from "react-dom"
+
+
+
 export default function NavBar() {
   return (
     <>
       <header>
         <h1>Ur Pantry Ur Way</h1>
       </header>
+ <Link to={'/'}></Link>
+ <Link to={'/'}>
+            Northwest <span>Animal Hospital</span>
+            </Link>
     </>
   );
 }
