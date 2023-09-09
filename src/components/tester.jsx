16513d59b5269501b49fa68589c05abec643afcc
xavierrice <xavier.rice@gmail.com>
2023-09-09 13:48:15 -0400

    starting fetch for pantry

--- src/components/tester.jsx
+++ src/components/tester.jsx
@@ -1,6 +1,22 @@
-
+import { useState, useEffect } from "react";
 
 const tester = () => {
+
+const [pantry, setPantry] = useState({})
+
+  useEffect(() => {
+
+    fetch(``)
+      .then(r => r.json())
+      .then(data => setAllVideos(data))
+      .catch(err => console.log(err))
+  }, [])
+
+
+
+
+
+
 
 return (
     <h1>Hello from X</h1>
