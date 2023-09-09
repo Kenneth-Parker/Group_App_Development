3215314f44ed5bd4e65fe498d825d8ea05b7cc8e
anickacodes <anickalewis@pursuit.org>
2023-09-09 17:30:10 -0400

    rendered routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
 
 //components
-import tester from './components/tester'
-import NavBar from './components/NavBar'
-
+import tester from "./components/tester";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-  <NavBar />
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <Router>
+        <NavBar />
+        <Routes>
+        
+          <Route></Route>
+          <Route></Route>
+        </Routes>
+
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
