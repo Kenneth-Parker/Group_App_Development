8b7cb2cf9ea566621c523059702585a4135d6d30
anickacodes <anickalewis@pursuit.org>
2023-09-11 18:28:18 -0400

    using 1 database

--- src/components/IngredientDropdownList.jsx
+++ src/components/IngredientDropdownList.jsx
@@ -1,33 +1,16 @@
 import React, { useEffect, useState } from "react";
 import ingredients from "./pantryitems";
-import pantry from "../pantryDb/pantry.json";
 
-const IngredientDropdownList = ( {onSelect}) => {
+
+const IngredientDropdownList = ({ onSelect }) => {
   const [allIngredients, setAllIngredients] = useState([]);
 
   useEffect(() => {
 
-    let pantryIngredients;
+     setAllIngredients(ingredients);
+   }, []);
 
-    if (Array.isArray(pantry.ingredients)) {
-      // If pantry.ingredients is already an array, use it as-is
-      pantryIngredients = pantry.ingredients;
-    } else if (typeof pantry.ingredients === "object") {
-      // If pantry.ingredients is an object, stringify and parse it to an array
-      pantryIngredients = Object.values(pantry.ingredients);
-    } else {
-      // If pantry.ingredients is neither an array nor an object, use an empty array
-      pantryIngredients = [];
-    }
-
-    const combinedIngredients = [
-        ...new Set([...ingredients, ...pantryIngredients]),
-      ];
-      
-    setAllIngredients(combinedIngredients);
-  }, []);
-
-  return(
+  return (
     <select onChange={(e) => onSelect(e.target.value)}>
       {allIngredients.map((ingredient, index) => (
         <option key={index} value={ingredient}>
