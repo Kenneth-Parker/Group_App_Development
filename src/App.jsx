49f0ad4692e3fb1f4aa7e110de8c4b652394c104
anickacodes <anickalewis@pursuit.org>
2023-09-13 23:37:56 -0400

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { useState } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
 
-import NavBar from './components/NavBar';
-import PantryList from './components/PantryList';
-import PantryItemSelector from './components/PantryItemSelector';
-import EdamamList from './components/MealApi';
-import MealApi from './components/MealApi';
-import ShoppingForm from './components/ShoppingForm';
-// import IngredientDropdownList from './components/IngredientDropdownList';
+import NavBar from "./components/NavBar";
+import PantryList from "./components/PantryList";
+import PantryItemSelector from "./components/PantryItemSelector";
+import EdamamList from "./components/MealApi";
+import ShoppingForm from "./components/ShoppingForm";
+
 
 function App() {
   const [selectedItems, setSelectedItems] = useState([]);
@@ -19,22 +18,38 @@ function App() {
     <>
       <Router>
         <NavBar />
-       
+
         <Routes>
-          <Route path="/" element={ <div> <PantryItemSelector selectedItems={selectedItems} setSelectedItems={setSelectedItems} /> <PantryList selectedItems={selectedItems} /> <EdamamList selectedItems={selectedItems} /> </div>}/>
+          <Route
+            path="/"
+            element={
+              <div>
+                {" "}
+                <PantryItemSelector
+                  selectedItems={selectedItems}
+                  setSelectedItems={setSelectedItems}
+                />{" "}
+                <PantryList selectedItems={selectedItems} />{" "}
+                <EdamamList selectedItems={selectedItems} />{" "}
+              </div>
+            }
+          />
           <Route path="/about" element={<h4>yup</h4>} />
-          <Route path="/contact" element={<p>"therewasafarmerhadadogandbingowashisnameo"</p>} />
-          <Route path="/shopping List" element={<ShoppingForm shoppingList={shoppingList} setShoppingList={setShoppingList}/>}/>
+          <Route
+            path="/contact"
+            element={<p>"therewasafarmerhadadogandbingowashisnameo"</p>}
+          />
+          <Route
+            path="/shopping List"
+            element={
+              <ShoppingForm
+                shoppingList={shoppingList}
+                setShoppingList={setShoppingList}
+              />
+            }
+          />
         </Routes>
       </Router>
-
-      {/* <PantryItemSelector selectedItems={selectedItems} setSelectedItems={setSelectedItems} /> */}
-      {/* <ShoppingForm/> */}
-
-
-      {/* <IngredientDropdownList /> */}
-      
-
     </>
   );
 }
